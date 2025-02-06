e42c0eb172e1ce2e70ca7846c36a6cf23cf95256
SihaoCheng123 <sihao.cheng@estudiante.epsum.school>
2025-02-06 22:48:08 +0100

    Tabs layout con recycler hecho

---
+++ app/src/main/java/com/example/mealplanner/ui/FragmentDetailedRecipeSteps.java
@@ -0,0 +1,53 @@
+package com.example.mealplanner.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mealplanner.R;
+import com.example.mealplanner.model.recycler.stepsSimple.StepsSimpleAdapter;
+import com.example.mealplanner.model.recycler.stepsSimple.StepsSimpleEventModel;
+
+import java.util.ArrayList;
+
+public class FragmentDetailedRecipeSteps extends Fragment {
+
+    private StepsSimpleAdapter adapter;
+    private ArrayList<StepsSimpleEventModel> stepsList = new ArrayList<>();
+
+    public FragmentDetailedRecipeSteps() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detailed_recipe_steps, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        setStepsEventModels();
+        adapter = new StepsSimpleAdapter(view.getContext(), stepsList);
+        RecyclerView recyclerView = view.findViewById(R.id.stepsRecyclerDR);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void setStepsEventModels(){
+        String [] stepsDescriptions = getResources().getStringArray(R.array.step_description_cv);
+        for (int i = 0; i < stepsDescriptions.length; i++){
+            stepsList.add(new StepsSimpleEventModel(String.valueOf(i+1), stepsDescriptions[i]));
+        }
+    }
+
+}
--- app/src/main/java/com/example/mealplanner/ui/components/RecipeDetailed.java
+++ app/src/main/java/com/example/mealplanner/ui/components/RecipeDetailed.java
@@ -2,29 +2,20 @@ package com.example.mealplanner.ui.components;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.mealplanner.R;
 import com.example.mealplanner.databinding.ActivityRecipeDetailedBinding;
-import com.example.mealplanner.model.recycler.ingredientsComplex.IngredientEventModel;
-import com.example.mealplanner.model.recycler.ingredientsSimple.IngredientSimpleAdapter;
-import com.example.mealplanner.model.recycler.ingredientsSimple.IngredientSimpleEventModel;
-import com.example.mealplanner.model.recycler.stepsSimple.StepsSimpleAdapter;
-import com.example.mealplanner.model.recycler.stepsSimple.StepsSimpleEventModel;
-import com.example.mealplanner.ui.auth.Login;
+import com.example.mealplanner.model.recycler.ViewPagerAdapterDR;
 import com.example.mealplanner.ui.fragments.HomeScreen;
-import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-import java.util.ArrayList;
 
 public class RecipeDetailed extends AppCompatActivity {
 
-    ArrayList<IngredientSimpleEventModel> ingredientsList = new ArrayList<>();
-    ArrayList<StepsSimpleEventModel> stepsList = new ArrayList<>();
     ActivityRecipeDetailedBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,51 +24,23 @@ public class RecipeDetailed extends AppCompatActivity {
         setContentView(R.layout.activity_recipe_detailed);
         binding = ActivityRecipeDetailedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.iconBackDR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
-                startActivity(intent);
-            }
+
+        binding.iconBackDR.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+            startActivity(intent);
         });
-//        setIngredientEventModels();
-//        IngredientSimpleAdapter ingredientSimpleAdapteradapter = new IngredientSimpleAdapter(this, ingredientsList);
-//        binding.ingredientsRecyclerDR.setAdapter(ingredientSimpleAdapteradapter);
-//        binding.ingredientsRecyclerDR.setLayoutManager(new LinearLayoutManager(this));
 
-        setStepsEventModels();
-        StepsSimpleAdapter stepsSimpleAdapter = new StepsSimpleAdapter(this, stepsList);
-        binding.ingredientsRecyclerDR.setAdapter(stepsSimpleAdapter);
-        binding.ingredientsRecyclerDR.setLayoutManager(new LinearLayoutManager(this));
-        binding.tabLayoutDR.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-
+        ViewPager2 viewPager2 = findViewById(R.id.viewPagerDR);
+        ViewPagerAdapterDR viewPagerAdapterDR = new ViewPagerAdapterDR(this);
+        binding.viewPagerDR.setAdapter(viewPagerAdapterDR);
+        new TabLayoutMediator(binding.tabLayoutDR, viewPager2, (tab, position) -> {
+            if (position == 0) {
+                tab.setText(R.string.ingredients);
+            } else {
+                tab.setText(R.string.steps);
             }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-    }
-    private void setIngredientEventModels(){
-        String[] ingredientNames = getResources().getStringArray(R.array.ingredient_name_cv);
-
-        for (int i = 0; i < ingredientNames.length; i++) {
-            ingredientsList.add(new IngredientSimpleEventModel(ingredientNames[i]));
-        }
+        }).attach();
     }
 
-    private void setStepsEventModels(){
-        String [] stepsDescriptions = getResources().getStringArray(R.array.step_description_cv);
-        for (int i = 0; i < stepsDescriptions.length; i++){
-            stepsList.add(new StepsSimpleEventModel(String.valueOf(i+1), stepsDescriptions[i]));
-        }
-    }
+
 }
