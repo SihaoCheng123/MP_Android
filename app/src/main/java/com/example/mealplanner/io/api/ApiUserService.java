a6a60b35b7b9a865f7ac6e1d8f863729343ea098
SihaoCheng123 <sihao.cheng@estudiante.epsum.school>
2025-03-05 19:53:14 +0100

    password changed successfully

--- app/src/main/java/com/example/mealplanner/io/api/ApiUserService.java
+++ app/src/main/java/com/example/mealplanner/io/api/ApiUserService.java
@@ -4,11 +4,13 @@ import com.example.mealplanner.model.data.Users;
 import com.example.mealplanner.model.dto.ApiDelivery;
 import com.example.mealplanner.model.dto.LoginRequest;
 import com.example.mealplanner.model.dto.LoginResponse;
+import com.example.mealplanner.model.dto.PasswordChangeRequest;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiUserService {
@@ -22,4 +24,7 @@ public interface ApiUserService {
 
     @PATCH("users/update/{id}")
     Call<Users> updateUser(@Body Users user, @Path("id") Long user_id);
+
+    @PUT("users/change-password/{id}")
+    Call<Users> changePassword(@Body PasswordChangeRequest passwordChangeRequest, @Path("id") Long user_id);
 }
--- app/src/main/java/com/example/mealplanner/ui/user/UserPassword.java
+++ app/src/main/java/com/example/mealplanner/ui/user/UserPassword.java
@@ -1,26 +1,73 @@
  package com.example.mealplanner.ui.user;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.mealplanner.R;
+import com.example.mealplanner.databinding.ActivityUserPasswordBinding;
+import com.example.mealplanner.io.api.ApiClient;
+import com.example.mealplanner.io.api.ApiUserService;
+import com.example.mealplanner.io.token.UserIdManager;
+import com.example.mealplanner.model.data.Users;
+import com.example.mealplanner.model.dto.PasswordChangeRequest;
 
-public class UserPassword extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-    @Override
+ public class UserPassword extends AppCompatActivity {
+
+    private ActivityUserPasswordBinding binding;
+
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_password);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityUserPasswordBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.btnUpdateUser.setOnClickListener(v -> changePassword());
+        binding.iconBackCP.setOnClickListener(v-> finish());
+    }
+
+    private void changePassword(){
+        ApiUserService apiUserService = ApiClient.getClient().create(ApiUserService.class);
+        UserIdManager userIdManager = new UserIdManager(this);
+        Call<Users> call = apiUserService.changePassword(getPasswordData(), userIdManager.getUserId());
+        call.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<Users> call, @NonNull Response<Users> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Password change successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error changing password", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Users> call, @NonNull Throwable throwable) {
+                Toast.makeText(getApplicationContext(), "Connection error", Toast.LENGTH_SHORT).show();
+            }
         });
+    }
+
+    private PasswordChangeRequest getPasswordData(){
+        String userOldPassword = binding.userOldPasswordInputCP.getEditText().getText().toString().trim();
+        String userNewPassword = binding.userNewPasswordInputCP.getEditText().getText().toString().trim();
+        String confirmPassword = binding.userConfirmNewPasswordInputCP.getEditText().getText().toString().trim();
+        PasswordChangeRequest newPasswordRequest = new PasswordChangeRequest();
+        if (userOldPassword.isEmpty() && userNewPassword.isEmpty() && confirmPassword.isEmpty()){
+            Toast.makeText(this, "Required data", Toast.LENGTH_SHORT).show();
+        }else {
+            newPasswordRequest.setOldPassword(userOldPassword);
+            if (!userNewPassword.equals(confirmPassword)){
+                Toast.makeText(this, "New passwords must match", Toast.LENGTH_SHORT).show();
+            }else {
+                newPasswordRequest.setNewPassword(userNewPassword);
+        }
+        }
+        return newPasswordRequest;
     }
 }
